eb236938adb15f0e64335bd3e3ea060489285328
GersonBenito <gersonbenito4@gmail.com>
2023-08-27 21:52:48 -0600

    feat: CRUD peluqueria de mascotas - WIP

---
+++ src/main/java/com/pet/peluqueriapet/PeluqueriaPet.java
@@ -0,0 +1,17 @@
+package com.pet.peluqueriapet;
+
+import com.pet.peluqueriapet.igu.Menu;
+
+/**
+ *
+ * @author Gerson Benito
+ */
+public class PeluqueriaPet {
+
+    public static void main(String[] args) {
+        // crear instancia del menu de JAVA Frame
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+}
